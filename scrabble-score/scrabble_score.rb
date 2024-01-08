de1a8fe8065d33bce1f3d04d809c0bc8f753bb66
Karan Chauhan <86901403+thekaranchauhan@users.noreply.github.com>
2024-01-08 14:14:03 -0500

    EX_scrabble-score

---
+++ scrabble-score/scrabble_score.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Scrabble
+  POINTS = {
+    'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1, 'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1,
+    'D' => 2, 'G' => 2,
+    'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
+    'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4,
+    'K' => 5,
+    'J' => 8, 'X' => 8,
+    'Q' => 10, 'Z' => 10
+  }.freeze
+
+  def initialize(word)
+    @word = word
+  end
+
+  def score
+    @word.chars.map { |char| POINTS[char.upcase] }.sum
+  end
+end
